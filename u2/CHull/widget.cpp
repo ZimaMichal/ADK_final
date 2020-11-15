5c07abb888875b6c873abffc283a2d8b59f129ab
rozumash <63002089+rozumash@users.noreply.github.com>
2020-11-15 22:47:27 +0100

    1.0

---
+++ u2/CHull/widget.cpp
@@ -0,0 +1,87 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include "algorithms.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    //Get points
+    QPolygon points = ui->Canvas->getPoints();
+
+    //Create Convex hull
+    QPolygon ch;
+    Algorithms alg;
+    QElapsedTimer timer;
+    timer.start();
+    //Jarvis Scan
+    if (ui->comboBox->currentIndex()==0)
+        ch = alg.jarvis(points);
+
+    //QHull
+    else if (ui->comboBox->currentIndex()==1)
+        ch = alg.qhull(points);
+
+    //Sweep line
+    else
+        ch = alg.sweepLine(points);
+
+    //Set Convex hull
+    ui -> Canvas -> setCH(ch);
+    ui->label_3->setText("Time:" + QString::number(timer.elapsed()) + "ms");
+
+    //Repaint screen
+    repaint();
+}
+
+void Widget::on_pushButton_2_clicked()
+{
+    //Clear CH
+    ui->Canvas->clearCH();
+}
+
+
+void Widget::on_pushButton_4_clicked()
+{
+    QSize size = ui->Canvas->size();
+    //Canvas height, width
+    int height = size.height();
+    int width = size.width();
+    QPolygon random_points;
+    int point_number = ui->lineEdit->text().toInt();
+
+    //Random points
+    if (ui->comboBox_2->currentIndex() == 0)
+        random_points = ui->Canvas->generateRandom(point_number, height, width);
+    //Grid
+    if (ui->comboBox_2->currentIndex() == 1)
+        random_points = ui->Canvas->generateGrid(point_number, height, width);
+    //Circle
+    if (ui->comboBox_2->currentIndex() == 2)
+        random_points = ui->Canvas->generateCircle(point_number, height, width);
+    //Show points
+        ui->Canvas->setPoints(random_points);
+        ui->Canvas->clearCH();
+        ui->Canvas->repaint();
+
+
+}
+
+void Widget::on_pushButton_3_clicked()
+{
+    //Clear all
+        ui->Canvas->clearPoints();
+        ui->Canvas->clearCH();
+        ui->Canvas->repaint();
+}
